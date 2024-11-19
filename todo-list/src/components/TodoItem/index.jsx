5c4e6e67041ad623f2f9860f6089d89b87271bbd
Simone Wight <sywight@gmail.com>
2024-11-19 20:15:32 +1000

    added items up to step 10

--- todo-list/src/components/TodoItem/index.jsx
+++ todo-list/src/components/TodoItem/index.jsx
@@ -1,7 +1,7 @@
 import './style.css'
 
 function TodoItem(props) {
-    return <div className="todo">{props.text}</div>;
+    return <div className="todo">{props.todo.text}</div>;
 }
 
 export default TodoItem;

03f96aa20e444e5bbebc01aa382fa01ee3eab839
Simone Wight <sywight@gmail.com>
2024-11-19 19:21:03 +1000

    pushing the latest updates for the codealong

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+
+function App() {
+    const [todos, setTodos] = useState([
+        "Learn about React",
+        "Meet a friend for lunch",
+        "Build a really cool todo app",
+    ]);
+
+    return (
+    <div>
+        <h1>My todo list</h1>
+        {todos.map()}
+    </div>
+    );
+}
+
+export default App;
